c4d5427d3ee6ac138484467499c97477abbeba9b
Abdulkadir Yaman <abdulkadiryaman@gmail.com>
2015-05-29 02:29:51 +0300

    removing unnecessary comments and changing to skydome address

--- nest-rest.ino
+++ nest-rest.ino
@@ -9,16 +9,13 @@
 
 #define SERVER_ADDRESS 0
 
-// Singleton instance of the radio
 RF22ReliableDatagram manager(SERVER_ADDRESS, 8, 0);
 
-// ethernet interface mac address, must be unique on the LAN
 static byte mymac[] = { 0x74, 0x69, 0x69, 0x2D, 0x30, 0x31 };
 
 byte Ethernet::buffer[700];
-static uint32_t timer;
 
-const char skydome[] PROGMEM = "192.168.2.120";
+const char skydome[] PROGMEM = "api.skydome.io";
 
 void setup() {
   Serial.begin(9600);
@@ -40,12 +37,12 @@ void setup() {
   Serial.println(F("setup() done"));
 }
 
-// called when the client request is complete
 static void my_callback (byte statusCode, word off, word len) {
   Ethernet::buffer[off + 300] = 0;
   Serial.println((const char*) Ethernet::buffer + off);
 }
 
+const uint8_t charOffset PROGMEM = 48;
 void loop() {
   ether.packetLoop(ether.packetReceive());
 
@@ -54,17 +51,15 @@ void loop() {
     uint8_t len = 2;
     uint8_t from;
 
-    if (manager.recvfromAck(buf, &len, &from)) { 
+    if (manager.recvfromAck(buf, &len, &from)) {
 
-      char message[] = {'{', '"', 'i', 'd', '"', ':',   '"', 'a', 'b', 'c', 'd', 'e', 'f', 'g',   '"', ',', /*{"id":"abcdefg",*/
-                        '"', 'f', 'r', 'o', 'm',   '"', ':',  '"', (from + 48),   '"', ',', /*"from":"1",*/
-                        '"', 't', 'y', 'p', 'e',   '"', ':',   '"', (buf[0] + 48),   '"', ',', /*"type":"7",*/
-                        '"', 'v', 'a', 'l', 'u', 'e',   '"', ':',   '"', (buf[1] + 48),   '"', '}' /*"value":"0"*/
-                       };
-
+      const char message[] PROGMEM = {'{', '"', 'i', 'd', '"', ':',   '"', 'a', 'b', 'c', 'd', 'e', 'f', 'g',   '"', ',', /*{"id":"abcdefg",*/
+                                      '"', 'f', 'r', 'o', 'm',   '"', ':',  '"', (from + charOffset),   '"', ',', /*"from":"1",*/
+                                      '"', 't', 'y', 'p', 'e',   '"', ':',   '"', (buf[0] + charOffset),   '"', ',', /*"type":"7",*/
+                                      '"', 'v', 'a', 'l', 'u', 'e',   '"', ':',   '"', (buf[1] + charOffset),   '"', '}' /*"value":"0"*/
+                                     };
 
       ether.httpPost(PSTR("/data"), skydome, PSTR("Authorization: Token asdaasd"), message, my_callback);
-
     }
   }
 }
