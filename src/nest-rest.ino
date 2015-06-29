b843f7dde65924e88930b6425c95c247517810f2
Abdulkadir Yaman <abdulkadiryaman@gmail.com>
2015-06-30 02:32:29 +0300

    removing debug

--- src/nest-rest.ino
+++ src/nest-rest.ino
@@ -13,32 +13,36 @@ static byte mymac[] = { 0x74, 0x69, 0x69, 0x2D, 0x30, 0x31 };
 
 byte Ethernet::buffer[700];
 
-//const char skydome[] PROGMEM = "api.skydome.io";
-const char skydome[] PROGMEM = "192.168.2.120";
+const char skydome[] PROGMEM = "api.skydome.io";
+//const char skydome[] PROGMEM = "192.168.2.20";'
 
 void setup() {
   Serial.begin(9600);
-  if (manager.init())
-    Serial.println(F("init success"));
-  else
-    Serial.println(F("init failed"));
+  if (manager.init()){
+  //  Serial.println(F("init success"));
+  }
+  else {
+   // Serial.println(F("init failed"));
+  }
+  
+  if (ether.begin(sizeof Ethernet::buffer, mymac, 10) == 0) {
+ //   Serial.println(F("Failed to access Ethernet controller"));
+}
+  if (!ether.dhcpSetup()){
+ //   Serial.println(F("DHCP failed"));
+}
 
-  if (ether.begin(sizeof Ethernet::buffer, mymac, 10) == 0)
-    Serial.println(F("Failed to access Ethernet controller"));
-  if (!ether.dhcpSetup())
-    Serial.println(F("DHCP failed"));
-
-  if (!ether.dnsLookup(skydome))
-    Serial.println(F("DNS failed"));
-
+  if (!ether.dnsLookup(skydome)){
+ //   Serial.println(F("DNS failed"));
+}
   ether.hisport = 8080;
 
-  Serial.println(F("setup() done"));
+//  Serial.println(F("setup() done"));
 }
 
 static void responseCallback (byte statusCode, word off, word len) {
   Ethernet::buffer[off + 300] = 0;
-  Serial.println((const char*) Ethernet::buffer + off);
+ // Serial.println((const char*) Ethernet::buffer + off);
 }
 
 const uint8_t charOffset PROGMEM = 48;
@@ -57,12 +61,15 @@ void loop() {
         
       Serial.print("got data[1]:");
       Serial.println(data[1]);
-      const char message[] PROGMEM = {'{','"', 'a', 'n', 't',            '"', ':', '"', (from + charOffset),   '"', ',', 
-                                          '"', 't', 'y', 'p', 'e',       '"', ':', '"', (data[0] + charOffset), '"', ',', 
-                                          '"', 'v', 'a', 'l', 'u', 'e',  '"', ':', '"', (data[1] + charOffset), '"', '}' 
-                                     };
-
-      ether.httpPost(PSTR("/telemetry/plant/id/abcdefg"), skydome, PSTR("Authorization: Token asdaasd"), message, responseCallback);
+    char fromChar = from;
+    char data0 = data[0];
+    char data1 = data[1];
+       const char *message = '1323';
+    //  char message[] PROGMEM = {'{','"','a','"',':','"',from+charOffset,'"',',','"','t','"',':','"',data[0]+charOffset,'"',',',
+    //                                '"', 'v', '"',':','"',data[1]+charOffset,'"', '}' 
+    //                           };
+    
+      ether.httpPost(PSTR("/telemetry/plant/id/yaman"), skydome, PSTR("Authorization: Token asdaasd"), message, NULL);
     }
   }
 }
